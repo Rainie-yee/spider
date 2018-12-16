9364647b7f2585c1b09f3b5181c603e463a917dc
Rainie-yee <45841236+Rainie-yee@users.noreply.github.com>
2018-12-16 17:22:13 +0800

    解析影片信息

--- src/edu/csuft/rainie/spider/App.java
+++ src/edu/csuft/rainie/spider/App.java
@@ -1,6 +1,7 @@
 package edu.csuft.rainie.spider;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -27,13 +28,25 @@ public class App {
 			Elements es = doc.select(".grid_view .item");     //查找在grid_view内部的图片
 			System.out.println(es.size());
 			
+			//创建一个影片的列表
+			ArrayList<Film> list = new ArrayList<>();
 			for(Element e :es) {
+				Film f =new Film();
 				// 每一部影片
 				Element t = e.select(".title").first();   //.代表class,#代表id
 				//每一部影片的名字
 				String num = e.select(".star span").last().text();   //取某个范围内的最后一个标签
 				String c = e.select(".quote span").text();
 				System.out.println(t.text() + "," + num + "\n" + c);
+				
+//				f.title
+//				f.info
+//				f.rating
+//				f.num
+//				f.id
+//				f.poster
+//				f.quote
+//				list.add(f)
 			}
 			
 //			String title = doc.title();
