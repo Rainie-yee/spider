f9744abf19a94dbe2a6055bc30c905e4679d9900
Rainie-yee <45841236+Rainie-yee@users.noreply.github.com>
2018-12-16 17:21:42 +0800

    解析影片信息

--- src/edu/csuft/rainie/spider/App.java
+++ src/edu/csuft/rainie/spider/App.java
@@ -1,5 +1,12 @@
 package edu.csuft.rainie.spider;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * 程序的入口
  * 
@@ -7,8 +14,38 @@ package edu.csuft.rainie.spider;
  * 
  */
 public class App {
-    //	alt+/
+    //	alt+/ 代码提示
 	public static void main(String[] args) {
+		
+		//目标 URL
+		String url = "https://movie.douban.com/top250";
+				
+		//使用 JSOUP 抓取文档
+		try {
+			Document doc = Jsoup.connect(url).get();
+			
+			Elements es = doc.select(".grid_view .item");     //查找在grid_view内部的图片
+			System.out.println(es.size());
+			
+			for(Element e :es) {
+				// 每一部影片
+				Element t = e.select(".title").first();   //.代表class,#代表id
+				//每一部影片的名字
+				String num = e.select(".star span").last().text();   //取某个范围内的最后一个标签
+				String c = e.select(".quote span").text();
+				System.out.println(t.text() + "," + num + "\n" + c);
+			}
+			
+//			String title = doc.title();
+//			String data = doc.data();
+//			
+//			System.out.println(title);
+//			System.out.println(data);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
---
+++ src/edu/csuft/rainie/spider/Film.java
@@ -0,0 +1,42 @@
+package edu.csuft.rainie.spider;
+
+public class Film {
+/**
+ * 影片名称
+ * 
+ * @author rainie
+ * 
+ */
+	String title;
+	
+	/**
+	 * 相关信息
+	 */
+	String info;
+	
+	/**
+	 * 评分
+	 */
+	double rating;
+	
+	/**
+	 * 评分人数
+	 */
+	int num;
+	
+	/**
+	 * 排名
+	 */
+	int id;
+	
+	/**
+	 * 海报
+	 */
+	String poster;
+	
+	/**
+	 * 经典语录
+	 */
+	String quote;
+
+}
